1ee6a70e554df9c6610abfa1a36c844afe97694a
Bunma93 <bunma93@gmail.com>
2024-06-25 19:09:02 +0700

    Js-lab9

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+let a = 1;
+let b = 2;
+let c = a++;//c=1,a=2
+console.log(a)
+console.log(c)
+let d = ++c;//d=2,c=2
+console.log(c)
+console.log(d)
+let e = ++d + d++ + d;// 2+4+4 e=10, d=4
+console.log(d)
+console.log(e)
